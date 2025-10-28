2e60b7e307092b38fa6db6fcab70cef934b68b48
TacoTruckMan777 <jcruz91011@gmail.com>
2025-10-28 08:51:33 -0400

    Update test.py

--- test.py/test.py
+++ test.py/test.py
@@ -1,37 +1,29 @@
-def Casino(Q,first,second,third):
+def Casino(Q,first,second,third,machine):
     times=0
-    count_1=1
-    count_2=0
-    count_3=0
     while Q >= 1:
-        if count_1 == 1:
-            Q -= 1
-            first += 1
-            count_1 += 1
-            count_2 +=1
-            times += 1
-        if count_2 == 1:
-            Q -= 1
-            second += 1
-            count_2 +=1
-            count_3=1
-            times += 1
-        if count_3 == 1:
-            Q -= 1
-            third += 1
-            count_3=0
-            count_1=1
-            times += 1
-        if first == 35:
-            Q += 30
-            first == 0
-        if second == 100:
-            Q += 60
-            second == 0
-        if third == 10:
-            Q += 9
-            third == 0
-        if Q <= 0:
-            for i in range(1):
-                print(times)
-Casino(48,3,10,4)
+        quarter= quarter-1
+
+        if machine == 0:
+            Q-=1
+            machine+=1
+            first+=1
+        if machine==1:
+            Q-=1
+            machine+=1
+            second+=1
+        if machine == 2:
+            Q-=1
+            machine+=1
+            third+=1
+            machine+0
+        if first==35:
+            Q+=35
+            first=0
+        if second==100:
+            Q+=60
+            second=0
+        if third==10:
+            Q+=9
+            third=0
+Casino(49,3,10,9)
+
